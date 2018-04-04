dccdc362d5d532225ec2b5d0194de7827aaae5ef
123 <haint09.lsc@gmail.com>
2018-04-04 23:46:35 +0700

    Hey
    Hello

---
+++ hw1.js
@@ -0,0 +1,4 @@
+var name = "Ngo Thanh Hai";
+var phone = 0965234096;
+var email = "haint09.lsc@gmail.com";
+console.log(`${name}\n${phone}\n${email}`)
---
+++ hw2.js
@@ -0,0 +1,3 @@
+var x = 10;
+(x < 0) ? x *= -1 : x *= 2;
+console.log(x);
